7b282c58327cd47111859be30e1b139fa3ff3948
Cupidi <ampov.mario@gmail.com>
2020-06-02 17:40:56 +0200

    change url on scroll

--- js/main.js
+++ js/main.js
@@ -3,10 +3,26 @@
   "use strict";
 
   var gum = new Gumshoe('.nav-collapse a', {
-    reflow: true,
     offset: function () {
       return 80;
     }
   });
+
+  var anchor = null;
+
+  document.addEventListener('gumshoeActivate', function (event) {
+    var link = event.detail.link;
+    anchor = link.href.substring(link.href.indexOf("#"));
+  }, false);
+
+  var isScrolling;
+  window.addEventListener('scroll', function (event) {
+    window.clearTimeout(isScrolling);
+    isScrolling = setTimeout(function () {
+      if (history.replaceState && anchor) {
+        history.replaceState(null, null, anchor);
+      }
+    }, 66);
+  }, false);
 
 })();
