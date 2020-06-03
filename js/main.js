3c0195491f9123c1ea1bbc6f86b89e41a4176236
Cupidi <ampov.mario@gmail.com>
2020-06-03 16:29:28 +0200

    make this responsive!

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 
   "use strict";
 
+  // Nav menu mark active link
+
   var gum = new Gumshoe('.nav-collapse a', {
     offset: function () {
-      return 80;
+      return 95;
     }
   });
 
-  var anchor = null;
+  // Change url to match the scrolled part of the page
 
+  var anchor = null;
   document.addEventListener('gumshoeActivate', function (event) {
     var link = event.detail.link;
     anchor = link.href.substring(link.href.indexOf("#"));
   }, false);
-
   var isScrolling;
   window.addEventListener('scroll', function (event) {
     window.clearTimeout(isScrolling);
@@ -24,5 +26,16 @@
       }
     }, 66);
   }, false);
+
+  // Hide nav menu on link click
+
+  var navToggle = document.querySelector("#navToggle");
+  var nav = document.querySelector(".nav-collapse ul");
+  var links = nav.querySelectorAll("a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (e) {
+      navToggle.checked = false;
+    }, false);
+  }
 
 })();
